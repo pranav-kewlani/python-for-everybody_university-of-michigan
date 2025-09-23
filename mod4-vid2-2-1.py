08dbf4a7dbdb31a62ca38286b9f45ee21ada1ab7
Pranav Kewlani <psk719@gmail.com>
2025-09-23 18:58:38 +0530

    discovered the use of * operator with strings

--- mod4-vid2-2-1.py
+++ mod4-vid2-2-1.py
@@ -3,3 +3,16 @@ y=x=1+2**3/4*5 #original numberical expression
 print(x,y)
 
 #output: 11.0 11.0
+
+#using the + and * operators with strings
+
+first = "test"
+sec = 3
+thir = "ing"
+print ("first*sec =",first*sec)
+print("first+sec =",first+thir)
+
+"""	output:
+first*sec = testtesttest
+first+sec = testing
+"""
