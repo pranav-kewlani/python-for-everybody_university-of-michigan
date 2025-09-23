114120b0037c7daefb0b19a2a379df2d0cd9c647
Pranav Kewlani <psk719@gmail.com>
2025-09-23 19:45:42 +0530

    used modulus % operator to get the last digit(s) of the number

--- mod4-vid2-2-1.py
+++ mod4-vid2-2-1.py
@@ -13,6 +13,18 @@ trying floored division (//): 1.0
 trying modulus operator(%): 2.0
 """
 
+#using the modulus operator to obtain the last digit(s) of the number
+print ("the last digit in",x,"is",x%10)
+print ("the last two digits in",x,"are",x%100)
+#realised, modulus can also be used to identify the number of digits in a number
+print ("the last three digits in",x,"are",x%1000) #unable to formulate the logic as of now, but when the result of the modulus operation shows digits less than the number of 0s, the number of digits in the number = number of (0s - 1)
+
+"""	output:
+the last digit in 11.0 is 1.0
+the last two digits in 11.0 are 11.0
+the last three digits in 11.0 are 11.0
+"""
+
 #using the + and * operators with strings
 
 first = "test"
