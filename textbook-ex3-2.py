00235efb1bf31b0eaa78accb1b0d22b01144a3c9
Pranav Kewlani <psk719@gmail.com>
2025-09-26 10:12:00 +0530

    exercise 3.3 done, try-except already implemented in ex2.3 & ex3.2

---
+++ textbook-ex3-2.py
@@ -0,0 +1,5 @@
+#Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program
+
+#try-except already iplemented in ex3.2 & 2.3
+
+#will try to link the file here after learning file-handling
