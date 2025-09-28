dfebd8473b0329c2252e6039d12e338252d51c9f
Pranav Kewlani <psk719@gmail.com>
2025-09-28 15:09:10 +0530

    textbook ex6.4 done, learned string method count()

---
+++ textbook-ex6-4.py
@@ -0,0 +1,8 @@
+#Exercise 4: There is a string method called count that is similar to the function in the previous exercise. Read the documentation of this method at:
+
+strS="banana"
+print("a occurs",strS.count("a"),"times in banana")
+
+"""	output
+a occurs 3 times in banana
+"""
